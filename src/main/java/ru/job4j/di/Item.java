e19578c92822ee81c1a1f3493b9177e5b01a6339
Qafas <rustam_ussr@mail.ru>
2023-03-23 22:58:32 +0400

    0. Что такое DI? [#6876]

---
+++ src/main/java/ru/job4j/di/Item.java
@@ -0,0 +1,74 @@
+package ru.job4j.di;
+
+/*
+import lombok.Data;
+import ru.job4j.toone.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "items")
+@Data
+public class Item {
+
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private LocalDateTime created = LocalDateTime.now();
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = {@JoinColumn(name = "item_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
+    )
+    private List<User> participates = new ArrayList<>();
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item() {
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
+    }
+
+    public Item(int id, String name, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
+
+ */
+
